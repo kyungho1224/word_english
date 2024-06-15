e9f9ec00e7c439b787099bc9c5802dd6fa55d9fc
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-06-15 20:59:49 +0900

    Feature-#17 파이어베이스 구글 로그인 - 구글 로그인 구현 - 로그인 유저 Provider 관리 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:word_english/provider/package_info_provider.dart';
 import 'package:word_english/provider/study_item_provider.dart';
+import 'package:word_english/provider/user_provider.dart';
 import 'package:word_english/word_english_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => PackageInfoProvider()),
         ChangeNotifierProvider(create: (_) => StudyItemProvider()),
       ],
